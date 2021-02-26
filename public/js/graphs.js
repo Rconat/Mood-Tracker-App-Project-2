3701a28f8fbbc0478e878e5ee0e088cf4c902c01
kihBootCamp <kimberleyheuer@hotmail.com>
2021-02-26 07:29:30 -0600

    plot 2 graphs, line and group

--- public/js/graphs.js
+++ public/js/graphs.js
@@ -7,22 +7,29 @@ $(document).ready(() => {
         console.log(data)
 
         MoodByDate(data);
+        MoodWith(data);
 
     });
 
     let labelArray = [];
     let dataArray = [];
+    let labelGroupArray = [];
+    let dataWithOthers = [];
+    let dataNotWithOthers = [];
+    let dataEatenToday = [];
+    let dataNotEatenToday = [];
+    let dataTakenMeds = [];
+    let dataNotTakenMeds = [];
 
     function MoodByDate(data) {
-
-        var i=0;
+        var i = 0;
 
         data.every(element => {
             dataArray.push(element.mood_rating);
-            labelArray.push(element.createdAt.slice(0,10) );
+            labelArray.push(element.createdAt.slice(0, 10));
 
             ++i;
-            if (i>9)
+            if (i > 9)
                 return false;
 
             return true;
@@ -30,6 +37,46 @@ $(document).ready(() => {
 
         PlotGraph(labelArray, dataArray);
     }
+
+    function MoodWith(data) {
+        var i = 0;
+
+        data.every(element => {
+            labelGroupArray.push(element.createdAt.slice(0, 10));
+
+            if (element.with_others) {
+                dataWithOthers.push(element.mood_rating);
+            }
+            else {
+                dataNotWithOthers.push(element.mood_rating);
+            }
+
+            if (element.eaten_today) {
+                dataEatenToday.push(element.mood_rating);
+            }
+            else {
+                dataNotEatenToday.push(element.mood_rating);
+            }
+
+            if (element.medications_today) {
+                dataTakenMeds.push(element.medications_today);
+            }
+            else {
+                dataNotTakenMeds.push(element.medications_today);
+            }
+
+            labelArray.push(element.createdAt.slice(0, 10));
+
+            ++i;
+            if (i > 9)
+                return false;
+
+            return true;
+        });
+
+        PlotGroupGraph();
+    }
+
 
     function PlotGraph(labels, data) {
 
@@ -40,10 +87,56 @@ $(document).ready(() => {
                 // labels: ['Red'],
                 labels: labels,
                 datasets: [{
-                    label: 'Past Week Moods',
-                    // data: [5, 8, 2, 7, 6, 8, 10],
+                    label: 'Last 10 Days Mood Rating',
+                    fill: false,
                     data: data,
+                    backgroundColor: "#88d8b0",
+                    borderColor: "#88d8b0"
+                    // backgroundColor: [
+                    //     "rgba(105,51,153, 0.5)"
+                    // ],
+                    // borderColor: [
+                    //     "rgba(105,51,153, 0.5)"
+                    // ]
                 }]
+            },
+            options: {
+                responsive: false
+            }
+        });
+    }
+
+    function PlotGroupGraph() {
+
+        console.log(dataEatenToday);
+
+        var ctx = document.getElementById('myChart2').getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: labelGroupArray,
+                datasets: [
+                    {
+                        label: "With Others",
+                        backgroundColor: "#ff6f69",                        
+                        data: dataWithOthers
+                    },
+                    {
+                        label: "Not With Others",
+                        backgroundColor: "#ffcc5c",
+                        data: dataNotWithOthers
+                    }
+                    // {
+                    //     label: "Eaten Today",
+                    //     backgroundColor: "#88d8b0",
+                    //     data: dataEatenToday
+                    // },
+                    // {
+                    //     label: "Not Eaten Today",
+                    //     backgroundColor: "#ffcc5c",
+                    //     data: dataNotEatenToday
+                    // }
+                ]
             },
             options: {
                 responsive: false
