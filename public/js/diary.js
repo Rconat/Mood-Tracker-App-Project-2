ff6be75fe1890f7d2645669bede0d32eb19a1964
Kristina Al-ashqar <kristinaal-ashqar@Kristinas-MacBook-Air.local>
2021-02-25 18:47:05 -0600

    working on ajax call

--- public/js/diary.js
+++ public/js/diary.js
@@ -1,19 +1,25 @@
 console.log("diary.js")
+const weatherBtn = document.getElementById("findWeatherButon");
+const zipcodeInput = document.getElementById('zipCodeInput')
 
 
 
-const successCallback = (position) => {
 
-    console.log(position)
-
-
-}
-const errorcallback = (error) => {
-
-    console.log(error)
-    console.log("thiserror happened")
+$(weatherBtn).on('click', ()=>{
+    const queryURL = `https://api.openweathermap.org/data/2.5/weather?zip=${zipcodeInput.value}&appid=56aea7b174cb46d06def2c0892d14ff9`
 
 
-}
 
-navigator.geolocation.getCurrentPosition(successCallback , errorcallback)
+   
+    $.ajax({ url: queryURL, method: "GET"}).then(function (response) {
+        var icon = response.weather[0].icon
+        var iconUrl =  "http://openweathermap.org/img/w/" + icon + ".png"
+
+        $('weatherBox').attr('<img src = "http://openweathermap.org/img/wn/10d@2x.png">')
+            
+    });
+
+})
+
+
+
