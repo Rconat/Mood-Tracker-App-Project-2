fea10bcdb1e39117570ddaa1297588b876f20688
ryzecous <r3j9c89@gmail.com>
2021-02-25 21:59:24 -0500

    pseudo code for group

--- public/js/members.js
+++ public/js/members.js
@@ -1,7 +1,15 @@
+/* eslint-disable */
+
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
-    $(".member-name").text(data.email);
+    console.log(data)
   });
 });
+
+// populate and display week at a glance
+
+// populate latest diary entry
+
+// populate past 10 diary entries if we want
