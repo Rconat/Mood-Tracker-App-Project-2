b0835ed4fc5d7a7ca348d2073dceca9874ba21d7
kihBootCamp <kimberleyheuer@hotmail.com>
2021-02-26 11:12:19 -0600

    fix conflicts

--- public/js/members.js
+++ public/js/members.js
@@ -4,9 +4,6 @@ $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(data => {
-<<<<<<< HEAD
-    console.log(data)
-=======
     $(".member-name").text(data.email);
 
     console.log(data);
@@ -15,7 +12,6 @@ $(document).ready(() => {
       console.log(element.mood_rating);
       
     });
->>>>>>> kim-merge-6
   });
 });
 
