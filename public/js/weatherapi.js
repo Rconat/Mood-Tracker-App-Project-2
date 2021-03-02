deb3698977c70cc53929989ca029ae347d7fa5e8
ryzecous <r3j9c89@gmail.com>
2021-03-02 16:21:05 -0500

    weatherapi.js syntax => eslint

--- public/js/weatherapi.js
+++ public/js/weatherapi.js
@@ -1,21 +1,13 @@
-const zipcodeInput = document.getElementById('zip')
-$(document).on('change', zipcodeInput , function() {
+const zipcodeInput = document.getElementById("zip");
+// function to bring in api weather call and populate DOM with weather icon for current weather
+$(document).on("change", zipcodeInput, () => {
+  const queryURL =
+    "https://api.openweathermap.org/data/2.5/weather?zip=60534&appid=56aea7b174cb46d06def2c0892d14ff9";
 
-    const queryURL = `https://api.openweathermap.org/data/2.5/weather?zip=60534&appid=56aea7b174cb46d06def2c0892d14ff9`
-
-    console.log(queryURL)
-    $.ajax({ url: queryURL, method: "GET"})
-    
-    .then(function (response) {
-        console.log(response)
-        const icon = response.weather[0].icon
-        const iconUrl =  "http://openweathermap.org/img/w/" + icon + ".png"
-        $('#wicon').attr('src', iconUrl);
-    });
-    $('#wicon').removeClass('hide')
-   
+  $.ajax({ url: queryURL, method: "GET" }).then(response => {
+    const icon = response.weather[0].icon;
+    const iconUrl = "http://openweathermap.org/img/w/" + icon + ".png";
+    $("#wicon").attr("src", iconUrl);
   });
-
-  //need to ask about async await
-
-  
+  $("#wicon").removeClass("hide");
+});
